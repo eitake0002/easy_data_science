d3b9fdb3118d862d4607855b9173693f78ab9363
root <eiki.e.t.d@gmail.com>
2016-10-05 16:49:06 +0900

    add utils.sh

---
+++ utils.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Load this file.
+# . ./easy_data_science/utils.sh
+
+#--------------------------------------------------------------------
+# Common utils
+#--------------------------------------------------------------------
+
+# Exec python file without createing .pyc
+function py(){
+  python -B $1
+}
+
+#--------------------------------------------------------------------
+# pep8
+#--------------------------------------------------------------------
+
+# Install pep8 and autopep8 with pip
+function install-pep(){
+  pip install pep8
+  pip install autopep8
+}
+
+# Check pep8
+function pep-check(){
+  pep8 $1
+}
+
+# Auto refactor.
+function pep-auto(){
+  autopep8 -i $1
+}
