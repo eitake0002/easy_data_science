9a2e0e5a2cf4591d96faff17b05155b72ba7744b
eitake0002 <eiki.e.t.d@gmail.com>
2016-10-07 09:36:17 +0000

    Add generate_simple_df and edit

--- pandas_commands.py
+++ pandas_commands.py
@@ -5,9 +5,10 @@ import pandas as pd
 import os
 
 # ---------------------------------------------------------------------
-# Paste below env in .bash_profile
+# MySQL
 # ---------------------------------------------------------------------
 
+# Paste below variables to ~/.bash_profile first.
 """
 export db_host=db_host
 export db_name=database
@@ -16,36 +17,36 @@ export db_pass=pass
 export db_charset=utf8
 """
 
-# ---------------------------------------------------------------------
-# DB parameter settings.
-# ---------------------------------------------------------------------
 
-db_host = os.environ["db_host"]
-db_name = os.environ["db_name"]
-db_user = os.environ["db_user"]
-db_pass = os.environ["db_pass"]
-db_charset = os.environ["db_charset"]
+def mysql_connect():
+    """
+    MySQL connection.
+    Above parameters must be set to ~/.bash_profile first.
+    """
 
-# ---------------------------------------------------------------------
-# MySQL connection
-# ---------------------------------------------------------------------
+    db_host = os.environ["db_host"]
+    db_name = os.environ["db_name"]
+    db_user = os.environ["db_user"]
+    db_pass = os.environ["db_pass"]
+    db_charset = os.environ["db_charset"]
 
-connector = pymysql.connect(
-    host=db_host,
-    db=db_name,
-    user=db_user,
-    # passwd = user,
-    charset=db_charset,
-)
-
-# ---------------------------------------------------------------------
-# Get data
-# ---------------------------------------------------------------------
+    return pymysql.connect(
+        host=db_host,
+        db=db_name,
+        user=db_user,
+        # passwd = user,
+        charset=db_charset,
+    )
 
 
 def table_data(table):
     """
     Execute SQL with pandas.
+
+    Parameters
+    ----------
+    table : string
+        Table name.
     """
     qry = "SELECT * FROM {0}".format(table)
     return pd.read_sql(qry, connector)
@@ -54,6 +55,13 @@ def table_data(table):
 def table_data_chunk(table, chunksize):
     """
     Execute SQL with chunk.
+
+    Parameters
+    ----------
+    table : string
+        Table name.
+    chunksize : integer
+        Number of chunk size to process.
     """
     qry = "SELECT * FROM {0}".format(table)
     return pd.read_sql(qry, connector, chunksize=chunksize)
@@ -61,10 +69,12 @@ def table_data_chunk(table, chunksize):
 # ---------------------------------------------------------------------
 # csv
 # ---------------------------------------------------------------------
+
+
 def read_csv(csv_path):
     """
     Read csv
-    
+
     Prameters
     ---------
     csv_path : str
@@ -73,13 +83,30 @@ def read_csv(csv_path):
     pd.read_csv(csv_path)
 
 # ---------------------------------------------------------------------
+# Generate random/test data
+# ---------------------------------------------------------------------
+
+def series_simple_df(num):
+    """
+    Generate simple dataframe
+
+    Parameters
+    ----------
+    num : integer
+        Number of craete data.
+    """
+    return pd.DataFrame([i for i in range(num)])
+
+# ---------------------------------------------------------------------
 # Search data
 # ---------------------------------------------------------------------
+
+
 def more_than_num(df, col, num):
     """
-    Get more than num data from dataframe. 
+    Get more than num data from dataframe.
 
-    Ex: 
+    Ex:
         more_than_num(df, "id", 1)
 
     Parameters
@@ -92,6 +119,7 @@ def more_than_num(df, col, num):
         More than value
     """
     return df.query("{0} > {1}".format(col, num))
+
 
 def less_than_num(df, col, num):
     """
@@ -111,6 +139,7 @@ def less_than_num(df, col, num):
     """
     return df.query("{0} < {1}".format(col, num))
 
+
 def between_num(df, col, low, high):
     """
     Get between data from dataframe.
@@ -127,12 +156,6 @@ def between_num(df, col, low, high):
     low : integer
         Less than value
     high : integer
-	More than value
+        More than value
     """
     return df.query("{1} > {2} & {1} < {3}".format(col, low, high))
-
-if __name__ == '__main__':
-    df = table_data("articles")
-    between_num(df, "id", 10, 15)
-    print(more_than_num(df, "id", 10))
-    print(less_than_num(df, "id", 10))
