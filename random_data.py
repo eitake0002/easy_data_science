005e71750fa68614421c09b14b3fba6a5a4b9ecb
root <eiki.e.t.d@gmail.com>
2016-10-10 16:18:20 +0900

    add rand_int_df()

--- random_data.py
+++ random_data.py
@@ -5,16 +5,37 @@ Generate random data with numpy module.
 """
 
 import numpy as np
+import pandas as pd
 
-
-def generate_int(num, maximum):
+def rand_int(num, maximum):
     """
     Generate integer random data.
-    Ex:
-        generate_int(10, 9999)
+
+    >>> len(rand_int(100, 100))
+    100
+
     Parameters
     ----------
     num : integer
         Number of values to generate
     """
     return np.random.randint(0, maximum, num)
+
+def random_df_int(num, maximum):
+    """
+    Generate random int data into DataFrame.
+
+    >>> len(random_df_int(100, 100))
+    100
+
+    Parameters
+    ----------
+    num : integer
+        Number of values to generate.
+    """
+    random_data_np = np.random.randint(0, maximum, num)
+    return pd.DataFrame(random_data_np)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
