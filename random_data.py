51131aa1636e783a26baa8bdd3f3857b71b1170b
root <eiki.e.t.d@gmail.com>
2016-10-10 17:26:50 +0900

    add random_str()

--- random_data.py
+++ random_data.py
@@ -6,6 +6,7 @@ Generate random data with numpy module.
 
 import numpy as np
 import pandas as pd
+import random
 
 def rand_int(num, maximum):
     """
@@ -35,6 +36,23 @@ def random_df_int(num, maximum):
     """
     random_data_np = np.random.randint(0, maximum, num)
     return pd.DataFrame(random_data_np)
+
+def random_str(num):
+    """
+    Generate random string array.
+
+    Parameters 
+    ----------
+    num : integer
+        Number of values to generate data.
+    """
+    str_list = "asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLZXCVBNMQWERTYUIOP"
+    rand_str = ""
+
+    for i in range(num):
+        rand_str += random.choice(str_list)
+
+    return rand_str
 
 if __name__ == '__main__':
     import doctest
--- test_random_data.py
+++ test_random_data.py
@@ -1,12 +1,18 @@
 import unittest
 import random_data as rd
 import pandas as pd
+import numpy as np
 
 
 class TestRandomData(unittest.TestCase):
 
     def test_simple_df(self):
-        self.assertEqual(len(rd.generate_int(10, 9999)), 10)
+        data = rd.rand_int(10, 10)
+        self.assertEqual(isinstance(data, np.ndarray), True)
+
+    def test_random_str(self):
+        data = rd.random_str(10)
+        self.assertEqual(isinstance(data, str), True)
 
 if __name__ == '__main__':
     unittest.main()
