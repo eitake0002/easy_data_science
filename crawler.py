9b00dd3362e1282408cdac9048c1833091b52182
eitake0002 <eiki.e.t.d@gmail.com>
2018-10-06 19:13:54 +0900

    Add RSS functions

--- crawler.py
+++ crawler.py
@@ -2,8 +2,12 @@
 Crawling and scraping.
 """
 
+import feedparser
 import urllib.request
 from bs4 import BeautifulSoup
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 
 def get_main_text(url):
     f = urllib.request.urlopen(url)
@@ -13,8 +17,49 @@ def get_main_text(url):
     return soup.get_text()
 
 
+def feed_rss(rss_url, description=False):
+    """Crawl rss
+
+    Parameters
+    ----------
+    rss_url : str
+        target rss url
+
+    Return
+    ------
+    RSS data : list
+        title, link, description
+
+    """
+    d = feedparser.parse(rss_url)
+
+    entry_list = []
+    if description is True:
+        for e in d["entries"]:
+            entry_data = (e["title"], e["link"], e["description"])
+            entry_list.append(entry_data)
+
+    else:
+        for e in d["entries"]:
+            entry_data = (e["title"], e["link"])
+            entry_list.append(entry_data)
+
+    return entry_list
+
+
+def feed_rss_list(rss_list_file):
+
+    with open(rss_list_file) as f:
+        rss_list = f.readlines()
+
+    rss_all_list = []
+    for i in rss_list:
+        rss_data = feed_rss(i)
+        rss_all_list.append(rss_data)
+
+    return rss_all_list
+
+
 if __name__ == '__main__':
-    url = "http://stackoverflow.com/questions/2023893/python-3-get-http-page"
-    url = "http://qiita.com/FGtatsuro/items/f45c349e06d6df95839b"
-    print(get_main_text(url))
-    
+    res = feed_rss_list("rss_list.csv")
+    pp.pprint(res)
