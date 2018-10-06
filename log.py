6171c2e85ec5e98ceb3a73e5aa1d467a80ed3524
eitake0002 <eiki.e.t.d@gmail.com>
2018-10-06 18:29:45 +0900

    Add ip address manipulation

--- log.py
+++ log.py
@@ -7,6 +7,10 @@ What you can do...
 """
 
 import boto3
+import re
+import pandas as pd
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def list_bucket_names():
@@ -67,7 +71,52 @@ def download_file(file_name, bucket_name, key_name):
     print("key    : {0}".format(key_name))
 
 
+def extract_ip_address(log_file):
+    """Extract ip address from log file
+
+    Parameter
+    ---------
+    log_file : 
+        log file
+
+    Return
+    ------
+    list : ip address list
+    """
+    with open(log_file) as f:
+        content = f.readlines()
+
+    ip_list = []
+    for i in content:
+        ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', i)
+        ip_list.append(ip[0])
+
+    return ip_list
+
+
+def convert_from_list_to_df(list_val):
+    """Convert from lsit to DataFrame"""
+
+    return pd.DataFrame(list_val)
+
+
+def grouping(df_data, col_num=0):
+    """Counting
+
+    Parameters
+    ----------
+    df_data : DataFrame
+
+    col_num : integer
+        Target column to count. 
+
+    """
+    pp.pprint(df_data.groupby(col_num).size())
+
+
 if __name__ == '__main__':
     # upload_file()
     # upload_file("test.log", "log-files-1234", "test.log")
-    download_file("test.log", "log-files-1234", "test.log")
+    # download_file("access_log", "log-files-1234", "access_log")
+    ip_list = extract_ip_address("access_log")
+    convert_from_list_to_df(ip_list)
